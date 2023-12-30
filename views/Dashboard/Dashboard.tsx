dc84107452f0397963c5f621fe2a410c65c940fc
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-12-31 01:32:21 +0600

    maybe done. but pagination not done

--- views/Dashboard/Dashboard.tsx
+++ views/Dashboard/Dashboard.tsx
@@ -3,12 +3,12 @@ import {Eyebrow, Gutter,Button,Card,Nav} from "payload/components/elements"
 import { GetServerSideProps } from 'next';
 import styles from  './Dashboard.module.css'
 import ProductCard from './ProductCard/ProductCard';
-import { Product } from '../../payload-types';
+import { Product, Sale } from '../../payload-types';
 import axios from 'axios';
 import apiurl from '../../apiurl';
 const Dashboard = (props) => {
-  const [monthSale, setmonthSale] = useState('')
-  const [weekSale, setweekSale] = useState('')
+  const [monthSale, setmonthSale] = useState(0)
+  const [weekSale, setweekSale] = useState(0)
   const [products, setproducts] = useState<Array<Product>>([])
   useEffect(() => {
     axios(`${apiurl}/api/product`).then((res)=>{
@@ -21,6 +21,29 @@ const Dashboard = (props) => {
 
       }
     })
+    axios(`${apiurl}/api/sales`).then((res)=>{
+      if(res.status==200){
+        res.data.docs.map((v:Sale,i)=>{
+          const tempDate = new Date(v.createdAt)
+          const today = new Date()
+          if(((today.getTime()-tempDate.getTime())/(1000*60*60*24*7))<1){
+            setweekSale(p=> p+1)
+          }
+        })
+      }
+    })
+    axios(`${apiurl}/api/sales`).then((res)=>{
+      if(res.status==200){
+        res.data.docs.map((v:Sale,i)=>{
+          const tempDate = new Date(v.createdAt)
+          const today = new Date()
+          if(((today.getTime()-tempDate.getTime())/(1000*60*60*24*30))<1){
+            setmonthSale(p=> p+1)
+          }
+        })
+      }
+    })
+    
   }, [])
   
   return (
@@ -31,14 +54,14 @@ const Dashboard = (props) => {
         <div className={styles.grid}>
           <div className="">
           <div className={styles.card}>
-            <p style={{fontSize:50,fontWeight:700}}>100</p>
+            <p style={{fontSize:50,fontWeight:700}}>{weekSale}</p>
             <p style={{marginTop:5}}>Sales This Week</p>
           </div>
           </div>
           <div className="">
             <div className={styles.card}>
-              <p style={{fontSize:50,fontWeight:700}}>100</p>
-              <p style={{marginTop:5}}>Sales This Month</p>
+              <p style={{fontSize:50,fontWeight:700}}>{monthSale}</p>
+              <p style={{marginTop:5}}>Sales Last 1 Month</p>
             </div>
           </div>
           
