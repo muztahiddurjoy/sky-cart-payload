3c3dfdddf29b5b9cd532a4b9c909e73f12159cf6
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-01-03 23:21:38 +0600

    new build

--- dist/server.js
+++ dist/server.js
@@ -76,16 +76,16 @@ var start = function () { return __awaiter(void 0, void 0, void 0, function () {
                     server.get('*', function (req, res) { return nextHandler_1(req, res); });
                     nextApp.prepare().then(function () {
                         console.log('Next.js started');
-                        server.listen(process.env.PORT, function () { return __awaiter(void 0, void 0, void 0, function () {
+                        server.listen(449, function () { return __awaiter(void 0, void 0, void 0, function () {
                             return __generator(this, function (_a) {
-                                console.log("Server listening on ".concat(process.env.PORT, "..."));
+                                console.log("Server listening on 449...");
                                 return [2 /*return*/];
                             });
                         }); });
                     });
                 }
                 else {
-                    server.listen(process.env.PORT, function () { return __awaiter(void 0, void 0, void 0, function () {
+                    server.listen(449, function () { return __awaiter(void 0, void 0, void 0, function () {
                         return __generator(this, function (_a) {
                             switch (_a.label) {
                                 case 0:
